3a5c775b788ea54500731bcbaf0473a8ed094d1a
s32057 <s32057@pjwstk.edu.pl>
2025-03-07 15:00:51 +0100

    wall of "text"

--- Program.cs
+++ Program.cs
@@ -2,5 +2,8 @@
 Console.WriteLine("Hello, World!");
 for (int i = 0; i < 100; i++)
 {
-    Console.WriteLine(i);
+    for (int b = 0; b < 100; b++)
+    Console.Write((char)(i+(b*100)));
+    
+    Console.WriteLine("");
 }
